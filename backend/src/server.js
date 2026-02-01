abf9331f545f0a3895af06f5a2caf46a925a5b96
Priyanshu0017 <priyanshubodana9@gmail.com>
2026-02-01 16:57:12 +0530

    combine frontend and backend for deployment and production purpose

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,26 @@ import dotenv from "dotenv";
 import authroutes from "./routes/auth.route.js";
 import messageroutes from "./routes/message.route.js";
 dotenv.config();
+import path from "path"
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Chatterbox API RUNNING..."));
+const __dirname = path.resolve()
+
+
 app.use("/api/auth", authroutes);
 app.use("/api/messages", messageroutes);
+
+
+// make ready for deployment
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist" )))
+
+  app.get(/.*/,(_,res) => {res.sendFile(path.join(__dirname, "../frontend","dist","index.html" ))})
+}
+
+// the "*" was old syntax will no longer work on express latest version the now syntax was /.*/ with new concept page-to-regexp v6"
 
 app.listen(port, (req, res) =>
   console.log(`server is running on port : ${port}`),
